e9219b8e1ddec4821f06db801446a7a3f87e9a79
keefeharris <keefeharrisjr@gmail.com>
2021-03-28 18:14:26 -0400

    Update script.js
    If applied, an object was added to the array named pokemonList

--- js/script.js
+++ js/script.js
@@ -9,7 +9,16 @@ pokemonList = [
     height: 2.04,
     weight: 15.2,
     weaknesses: ['fire', 'psychic', 'flying', 'ice']
-  }
+  },
+  {
+    name: 'Charmander',
+    number: '#004',
+    type: ['fire'],
+    height: 2.0,
+    weight: 18.7,
+    weaknesses: ['water', 'ground', 'rock']
+  },
+  
 ];
 
 console.log(pokemonList.length);
