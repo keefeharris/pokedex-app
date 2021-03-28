7c293ab915c0696e09beb8d6c16d369ff6b49b0f
keefeharris <keefeharrisjr@gmail.com>
2021-03-28 18:11:06 -0400

    Update script.js
    If applied, 1 object was added to the pokemonList array.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 let pokemonList = [];
 //an array name pokemonList is created
 
+pokemonList = [
+  {
+    name: 'Bulbasaur',
+    number: '#001',
+    type: ['grass', 'poison'],
+    height: 2.04,
+    weight: 15.2,
+    weaknesses: ['fire', 'psychic', 'flying', 'ice']
+  }
+];
+
+console.log(pokemonList.length);
