206c095fa481c6887d3f506e31c2ed7bd0bd85c0
keefeharris <keefeharrisjr@gmail.com>
2021-03-30 00:58:05 -0400

    Update script.js
    if applied, global variable named pokemonRepository was created

--- js/script.js
+++ js/script.js
@@ -1,43 +1,44 @@
-let pokemonList = [];
-//an array name pokemonList is created
+let pokemonRepository = (function() {
+  let pokemonList = [];
+  //an array name pokemonList is created
 
-pokemonList = [
-  //object is added to array
-  {
-    name: 'Bulbasaur',
-    number: '#001',
-    type: ['grass'],
-    height: 2.04,
-    weight: 15.2,
-    weaknesses: ['fire', 'psychic', 'flying', 'ice']
-  },
-  //object is added to array
-  {
-    name: 'Charmander',
-    number: '#004',
-    type: ['fire'],
-    height: 2.0,
-    weight: 18.7,
-    weaknesses: ['water', 'ground', 'rock']
-  },
-  //object is added to array
-  {
-    name: 'Squirtle',
-    number: '#007',
-    type: ['water'],
-    height: 1.08,
-    weight: 19.8,
-    weaknesses: ['grass', 'electric']
-  }
-];
+  pokemonList = [
+    //object is added to array
+    {
+      name: 'Bulbasaur',
+      number: '#001',
+      type: ['grass'],
+      height: 2.04,
+      weight: 15.2,
+      weaknesses: ['fire', 'psychic', 'flying', 'ice']
+    },
+    //object is added to array
+    {
+      name: 'Charmander',
+      number: '#004',
+      type: ['fire'],
+      height: 2.0,
+      weight: 18.7,
+      weaknesses: ['water', 'ground', 'rock']
+    },
+    //object is added to array
+    {
+      name: 'Squirtle',
+      number: '#007',
+      type: ['water'],
+      height: 1.08,
+      weight: 19.8,
+      weaknesses: ['grass', 'electric']
+    }
+  ];
 
-//forEach() uses pokemonList[] to iterate objects like the for loop
-//a function declaration is passed as a parameter
-pokemonList.forEach(function(pokemon) {
-  document.write(pokemon.name + ' is a ' + pokemon.type + ' type pokemon.<br>');
-  //What forEach() does is pass this function for each element in the array
-})
-
+  //forEach() uses pokemonList[] to iterate objects like the for loop
+  //a function declaration is passed as a parameter
+  pokemonList.forEach(function(pokemon) {
+    document.write(pokemon.name + ' is a ' + pokemon.type + ' type pokemon.<br>');
+    //What forEach() does is pass this function for each element in the array
+  })
+})();
 
 
 
