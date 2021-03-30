b6720a628aec611395de60b83ac762ea3ed0c186
keefeharris <keefeharrisjr@gmail.com>
2021-03-30 00:51:19 -0400

    Update script.js
    if applied, for loops are commented out because of the forEach loop being used for the same purpose

--- js/script.js
+++ js/script.js
@@ -31,14 +31,6 @@ pokemonList = [
   }
 ];
 
-console.log(pokemonList.length);
-//logs on to the console the length of my array
-
-/*
-the for loop says if 'i' is less than the length of values in the pokemonList array,
-keep adding 1 to i
-*/
-
 //forEach() uses pokemonList[] to iterate objects like the for loop
 //a function declaration is passed as a parameter
 pokemonList.forEach(function(pokemon) {
@@ -47,28 +39,56 @@ pokemonList.forEach(function(pokemon) {
 })
 
 
-for(let i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + ' is a ' + pokemonList[i].type + ' type pokemon. <br>');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log(pokemonList.length);
+//logs on to the console the length of my array
+
+/*
+the for loop says if 'i' is less than the length of values in the pokemonList array,
+keep adding 1 to i
+*/
+
+//for(let i = 0; i < pokemonList.length; i++) {
+  //document.write(pokemonList[i].name + ' is a ' + pokemonList[i].type + ' type pokemon. <br>');
   //the document.write function write text directly to html
   //pokemonList[i].name calls the name of the pokemon positioned at number "i" in the pokemonList array
   //the other charachters are strings that form a sentence in html
   //<br> element produce a line break in a sentence
-}
+//}
 
-for(let i = 0; i < pokemonList.length; i++) {
+//for(let i = 0; i < pokemonList.length; i++) {
   //for loop goes through each object in array
-  if (pokemonList[i].weaknesses.length > 2) {
+  //if (pokemonList[i].weaknesses.length > 2) {
     //the length of the weakness array is selected from pokemonList array to carry out condition 
     //template literal were used to specify strings instead of "+" and "''".
     /*
     if the object has more than 2 weakness the first document.write statement goes to the document.
     if the object has less than 2 weakness the second document.write statment goes to the document.
     */
-    document.write(`${pokemonList[i].name} is vulnerable to at least 3 type of pokemon. <br>`);
-  } else {
-    document.write(`${pokemonList[i].name} is vulnerable to a maximum of 2 types of pokemon. <br>`);
-  }
-}
+    //document.write(`${pokemonList[i].name} is vulnerable to at least 3 type of pokemon. <br>`);
+  //} else {
+    //document.write(`${pokemonList[i].name} is vulnerable to a maximum of 2 types of pokemon. <br>`);
+  //}
+//}
 
 
 
