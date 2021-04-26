65cdcfaffcc6030144136741a95c9e3c1a0af7e9
keefeharris <keefeharrisjr@gmail.com>
2021-04-26 17:26:24 -0400

    Update script.js
    if applied, code was cleaned up in script.js to implement promises

--- js/script.js
+++ js/script.js
@@ -1,180 +1,95 @@
-let pokemonRepository = (function() {
+let pokemonRepository = (function () {
   let pokemonList = [];
 
   pokemonList = [
     {
-      name: 'Bulbasaur',
-      number: '#001',
-      type: 'grass',
-      weaknesses: ['fire', 'psychic', 'flying', 'ice']
+      name: "Bulbasaur",
+      number: "#001",
+      type: "grass",
+      weaknesses: ["fire", "psychic", "flying", "ice"],
     },
-    
+
     {
-      name: 'Charmander',
-      number: '#004',
-      type: 'fire',
-      weaknesses: ['water', 'ground', 'rock']
+      name: "Charmander",
+      number: "#004",
+      type: "fire",
+      weaknesses: ["water", "ground", "rock"],
     },
-    
+
     {
-      name: 'Squirtle',
-      number: '#007',
-      type: 'water',
-      weaknesses: ['grass', 'electric']
-    }
+      name: "Squirtle",
+      number: "#007",
+      type: "water",
+      weaknesses: ["grass", "electric"],
+    },
   ];
 
-  function showDetails(pokemon) {
-      for (pokemon = 0; pokemon < pokemonList.length; pokemon++) {
-        console.log(pokemonList[pokemon].name);
-    }
-  }
-  
-  function addListItem(pokemon){
-    let pokemonList = document.querySelector('.pokemon-list');
-    let listPokemon = document.createElement('li');
-    let button = document.createElement('button');
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listPokemon = document.createElement("li");
+    let button = document.createElement("button");
     button.innerText = pokemon.name;
-    button.classList.add('.button-class');
+    button.classList.add(".button-class");
     listPokemon.appendChild(button);
     pokemonList.appendChild(listPokemon);
-    button.addEventListener('click', showDetails);
   }
 
-  function pokeFilter() {
-    let filt = pokemonList.filter(function (pokemon) {
-      return pokemon.name === 'Dratini';
-    });
-    console.log(filt);
-  }
-  
   function add(pokemon) {
-    if(typeof pokemon === 'object') {
+    if (typeof pokemon === "object") {
       console.log(
-        'Is this a pokemon? ' + Object.keys(pokemonList[0]).every(property => property in pokemon)
+        "Is this a pokemon? " +
+          Object.keys(pokemonList[0]).every((property) => property in pokemon)
       );
-      if (Object.keys(pokemonList[0]).every(property => property in pokemon)) {
-        alert('New pokemon named ' + pokemon.name + ' has been added to Pokedex.');
+      if (
+        Object.keys(pokemonList[0]).every((property) => property in pokemon)
+      ) {
+        alert(
+          "New pokemon named " + pokemon.name + " has been added to Pokedex."
+        );
       }
       pokemonList.push(pokemon);
     } else {
-      alert(pokemon.name + ' is not an applicable entry in the Pokedex!');
+      alert(pokemon.name + " is not an applicable entry in the Pokedex!");
     }
   }
-  
+
   function getAll() {
     return pokemonList;
   }
-  
+
   return {
-    showDetails: showDetails,
+    add: add,
     addListItem: addListItem,
-    pokeFilter: pokeFilter,
-    add: add, 
-    getAll: getAll
-  }
+    getAll: getAll,
+  };
 })();
 
-//console.log(pokemonRepository.getAll());
+console.log(pokemonRepository.getAll());
 
 pokemonRepository.add({
-  name: 'Dratini', 
-  number: '#147', 
-  type: 'dragon',
-  weaknesses: ['ice', 'dragon', 'fairy']
+  name: "Dratini",
+  number: "#147",
+  type: "dragon",
+  weaknesses: ["ice", "dragon", "fairy"],
 });
-
-//console.log(pokemonRepository.getAll()); 
-
-//console.log(pokemonRepository.pokeFilter());
 
 pokemonRepository.getAll().forEach(function (pokemon) {
   pokemonRepository.addListItem(pokemon);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
 /*
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addNewItem(pokemon);
-});
+function showDetails(pokemon) {
+    for (pokemon = 0; pokemon < pokemonList.length; pokemon++) {
+      console.log(pokemonList[pokemon].name);
+    }
+  }
+
+  pokeFilter: pokeFilter,
+
+  function pokeFilter() {
+    let filt = pokemonList.filter(function (pokemon) {
+      return pokemon.name === "Dratini";
+    });
+    console.log(filt);
+  }
 */
-
-
-
-
-
-
